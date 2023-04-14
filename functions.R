02a5c2e4e8d8dd32f15806a17004cfd7043f0b4b
Soren Rajani <sorenrajani@gmail.com>
2023-04-14 11:34:33 -0700

    added sqrt(odometer)

--- functions.R
+++ functions.R
@@ -212,6 +212,9 @@ wrangling_function<- function(data){
     
     new_data = mutate(new_data, countryOrigin = as.character(countryOrigin))%>%
     mutate_if(sapply(., is.character), as.factor) #factorizes <chr> variables
+    
+    new_data = new_data %>% mutate(sqrt_odometer = sqrt(odometer)) %>% select(-odometer)
+    
     new_data = subset(new_data, select = -c(year))
     return(new_data)
     }
